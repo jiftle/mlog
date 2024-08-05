87a48e575ea074cc09d67eea8d00fc53b0bd602e
ThinkBook14-G5-ARP <ThinkBook14-G5-ARP@AMD-Ryzen-7-7735H-with-Radeon-Graphics>
2024-08-05 13:49:31 +0800

    up depency

--- mlog/log.go
+++ mlog/log.go
@@ -8,12 +8,20 @@ func Log() *Logger {
 	if logger == nil {
 		cnf, err := loadConfig()
 		if err != nil {
-			panic(err)
+			logger = &Logger{
+				config: &Config{
+					Path:   "logs",
+					Level:  "debug",
+					Stdout: true,
+				},
+			}
+			logger.SetLevelStr("debug")
+		} else {
+			logger = &Logger{
+				config: cnf,
+			}
+			logger.SetLevelStr(cnf.Level)
 		}
-		logger = &Logger{
-			config: cnf,
-		}
-		logger.SetLevelStr(cnf.Level)
 	}
 	return logger
 }
